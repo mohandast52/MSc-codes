4c50df531aa4370a916dc61e52a636182957c7f7
mohandast52 <mohandast52@gmail.com>
2018-04-17 00:58:07 +0530

    p5js first push (of folders and p5 manager)

---
+++ p5js/project-1/bubble/bubble.js
@@ -0,0 +1,10 @@
+
+function setup() {
+	createCanvas(600, 600);
+}
+
+function draw() {
+	background(255, 0, 0);
+	ellipse(200, 200, 50, 50);
+	ellipse(200, 100, 50, 50);
+}
---
+++ p5js/project-1/sketch.js
@@ -0,0 +1,10 @@
+///<reference path="p5.global-mode.d.ts"/>
+
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+	background(255, 66, 10);
+	ellipse(200, 300, 50, 50);
+}
